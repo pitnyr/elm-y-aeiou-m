59bf621a515ea696e2f62b95ae6cff6ce50dc53a
Pit Capitain <pit@capitain.de>
2022-03-07 19:18:57 +0100

    Return Val from every function
    See https://pitnyr.github.io/elm-y-aeiou-m/making-of/basic.html#commit-2022-03-07-19-18
    
    We pass the "Val" returned from the last function call as input to the
    next function call. This guarantees the desired function call sequence.

--- src/Main.elm
+++ src/Main.elm
@@ -5,10 +5,17 @@ import Html as H
 
 main : H.Html msg
 main =
+    let
+        ( fourChars, _ ) =
+            get4Chars (Val 1)
+
+        ( when, _ ) =
+            nowOrLater (Val 0)
+    in
     H.div []
-        [ H.text <| "get2Chars: " ++ get2Chars (Val 1) (Val 2)
+        [ H.text <| "get4Chars: " ++ fourChars
         , H.br [] []
-        , H.text <| "nowOrLater: " ++ nowOrLater (Val 0)
+        , H.text <| "nowOrLater: " ++ when
         ]
 
 
@@ -21,21 +28,46 @@ type Val
 Currently the character is only dependent on the given parameter value.
 
 -}
-getChar : Val -> Char
+getChar : Val -> ( Char, Val )
 getChar (Val code) =
-    Char.fromCode (Char.toCode 'A' + modBy 26 code)
+    ( Char.fromCode (Char.toCode 'A' + modBy 26 code)
+    , Val (code + 1)
+    )
 
 
-get2Chars : Val -> Val -> String
-get2Chars val1 val2 =
-    String.fromChar (getChar val1) ++ String.fromChar (getChar val2)
+get2Chars : Val -> ( String, Val )
+get2Chars val0 =
+    let
+        ( a, val1 ) =
+            getChar val0
+
+        ( b, val2 ) =
+            getChar val1
+    in
+    ( String.fromChar a ++ String.fromChar b
+    , val2
+    )
 
 
-nowOrLater : Val -> String
-nowOrLater val =
-    case getChar val of
-        'Y' ->
-            "Now"
+get4Chars : Val -> ( String, Val )
+get4Chars val0 =
+    let
+        ( a, val1 ) =
+            get2Chars val0
 
-        _ ->
-            "Later"
+        ( b, val2 ) =
+            get2Chars val1
+    in
+    ( a ++ b
+    , val2
+    )
+
+
+nowOrLater : Val -> ( String, Val )
+nowOrLater val0 =
+    case getChar val0 of
+        ( 'Y', val1 ) ->
+            ( "Now", val1 )
+
+        ( _, val1 ) ->
+            ( "Later", val1 )
