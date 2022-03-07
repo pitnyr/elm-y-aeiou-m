ba0b8f06dc224a0092c55e55c7f37a1a84a7d961
Pit Capitain <pit@capitain.de>
2022-03-07 18:44:25 +0100

    Very first version: getChar : Char
    See https://pitnyr.github.io/elm-y-aeiou-m/making-of/basic.html#commit-2022-03-07-18-44
    
    Because Elm only has pure functions, "getChar" currently always
    returns the same character.

--- src/Main.elm
+++ src/Main.elm
@@ -5,4 +5,19 @@ import Html as H
 
 main : H.Html msg
 main =
-    H.text "Hi"
+    H.text get2Chars
+
+
+{-| Goal: return a random uppercase character
+
+Currently all we can do is to always return the same character...
+
+-}
+getChar : Char
+getChar =
+    'X'
+
+
+get2Chars : String
+get2Chars =
+    String.fromChar getChar ++ String.fromChar getChar
